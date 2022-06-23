f5ecb927119d25f886c7dc6ea28c2d159a3e5edc
ialej001 <ivan.alejandre@gmail.com>
2022-06-23 14:43:59 -0700

    init book interface

---
+++ src/models/book.tsx
@@ -0,0 +1,11 @@
+export interface Book {
+    title: string;
+    authors: string[];
+    publisher?: string;
+    imgUrl: ImgUrls;
+}
+
+interface ImgUrls {
+    smallThumbnail?: string;
+    thumbnail?: string;
+}
