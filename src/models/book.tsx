cf15c1593521b6acc2edb37fb1eeaa6a66d266af
ialej001 <ivan.alejandre@gmail.com>
2022-06-26 14:49:35 -0700

    expanded model

--- src/models/book.tsx
+++ src/models/book.tsx
@@ -1,11 +1,23 @@
 export interface Book {
   title: string;
+  subtitle: string;
   authors: string[];
-  publisher?: string;
+  publisher: string;
+  publishedDate: string;
   imgUrl: ImgUrls;
+  isbn: Isbn[];
+  description: string;
+  pageCount: number;
+  averageRating: number;
+  ratingCount: number;
 }
 
 interface ImgUrls {
-  smallThumbnail?: string;
-  thumbnail?: string;
+  smallThumbnail: string;
+  thumbnail: string;
+}
+
+interface Isbn {
+  type: string;
+  identifier: string;
 }
